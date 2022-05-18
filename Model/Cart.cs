25cf3e4a3b6b5322ded8db113a8276c0e6c9d1b1
gopikrishv <gopikrishvs@gmail.com>
2022-05-18 11:14:55 +0530

    commit for orderitem

---
+++ Model/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderItem.Model
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int MenuItemId { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+    }
+}
